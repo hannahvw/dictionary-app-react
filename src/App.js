9fc145db7d57bfd0ceef00003448be9375ec86d1
hannahvw <103385894+hannahvw@users.noreply.github.com>
2024-03-09 16:15:49 -0600

    Added github link

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      Dictionary App
+      <p>
+        Coded by Hannah Van Wyk and open-sourced on{" "}
+        <a href="https://github.com/hannahvw/dictionary-app-react">GitHub</a>.
+      </p>
+    </div>
+  );
+}
+
+export default App;
