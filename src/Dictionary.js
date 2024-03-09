3cbb7d2fc3d174f4d6623939afba561811ff189e
hannahvw <103385894+hannahvw@users.noreply.github.com>
2024-03-09 17:41:06 -0600

    Update Dictionary.js

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -8,9 +8,10 @@ export default function Dictionary() {
    }
     
     function search(event) {
-        event.preventDefault();
-        let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
-        axios.get(apiUrl).then(handleResponse);
+      event.preventDefault();
+      //api documentation: https://dictionaryapi.dev/
+      let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
+      axios.get(apiUrl).then(handleResponse);
     }
 
     function handleKeywordChange(event) {
