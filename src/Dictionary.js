0381cf5f8da2ad57e4e9e4702f6a3c24706b9098
hannahvw <103385894+hannahvw@users.noreply.github.com>
2024-03-11 13:59:34 -0500

    Added photos component

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,20 +2,30 @@ import React, { useState } from "react";
 import axios from "axios";
 import Results from "./Results";
 import "./Dictionary.css";
+import Photos from "./Photos";
 
 export default function Dictionary(props) {
   let [keyword, setKeyword] = useState(props.defaultKeyword);
   let [results, setResults] = useState(null);
   let [loaded, setLoaded] = useState(false);
+  let [photos, setPhotos] = useState(null);
 
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     setResults(response.data);
+  }
+
+  function handleImagesResponse(response) {
+    setPhotos(response.data.photos);
   }
 
   function search() {
     let apiKey = `2cacbf3044aeb9a87b5a33at06fco72a`;
     let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    let imagesApiKey = `2cacbf3044aeb9a87b5a33at06fco72a`;
+    let imagesApiUrl = `https://api.shecodes.io/images/v1/search?query=${keyword}&key=${imagesApiKey}`;
+    axios.get(imagesApiUrl).then(handleImagesResponse);
   }
 
   function handleSubmit(event) {
@@ -34,33 +44,32 @@ export default function Dictionary(props) {
 
   if (loaded) {
     return (
-    <div className="Dictionary">
-      <section>
-        <h2>What word do you want to look up?</h2>
-        <form className="input-group mb-3" onSubmit={handleSubmit}>
-          <input
-            type="search"
-            className="form-control"
-            autoFocus={false}
-            onChange={handleKeywordChange}
-            defaultValue={props.defaultKeyword}
-          />
-          <button
-            class="btn btn-outline-secondary"
-            type="Search"
-            id="button-addon2"
-          >
-            Search
-          </button>
-        </form>
-      </section>
-      <Results results={results} />
-    </div>
-  );
+      <div className="Dictionary">
+        <section>
+          <h2>What word do you want to look up?</h2>
+          <form className="input-group mb-3" onSubmit={handleSubmit}>
+            <input
+              type="search"
+              className="form-control"
+              autoFocus={false}
+              onChange={handleKeywordChange}
+              defaultValue={props.defaultKeyword}
+            />
+            <button
+              className="btn btn-outline-secondary"
+              type="Search"
+              id="button-addon2"
+            >
+              Search
+            </button>
+          </form>
+        </section>
+        <Results results={results} />
+        <Photos photos={photos} />
+      </div>
+    );
   } else {
     load();
     return "Loading...";
   }
-
-  
 }
